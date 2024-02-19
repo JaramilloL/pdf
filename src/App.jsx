cbd96572bc927a1c5b1e22ff7306e7fd1650ee73
JLJL <juanluisjaramillolopez@gmail.com>
2024-02-19 14:24:38 -0600

    agregamos estilos para mejorar el pdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 //para poder descargar el archivo de pdf creado necesitamos llamara una funcion de react-pdf
-import { PDFDownloadLink } from '@react-pdf/renderer'
+import { PDFDownloadLink, PDFViewer } from '@react-pdf/renderer'
 import PdfDocument from './components/PdfDocument'
 
 const App = () => {
   return (
     <div>
+    <PDFViewer>
+      <PdfDocument/>
+    </PDFViewer>
       <PDFDownloadLink document={<PdfDocument/>} fileName='ejemplo.pdf'>
         {
           //dentro creamos validacione para la descarga del atchivo //, url, error, blob
--- src/components/PdfDocument.jsx
+++ src/components/PdfDocument.jsx
@@ -1,5 +1,12 @@
 //importamos al libreria de react-pdf-render para empezar a crear el documento
-import { Document, Text, Page, Image, StyleSheet, View } from "@react-pdf/renderer";
+import {
+  Document,
+  Text,
+  Page,
+  Image,
+  StyleSheet,
+  View,
+} from "@react-pdf/renderer";
 
 import img from "../assets/react-pdf.jpg";
 
@@ -7,40 +14,135 @@ const PdfDocument = () => {
   //creamos los estilos
   const styles = StyleSheet.create({
     page: {
-      flexDirection: 'row',
-      backgroundColor: '#E4E4E4'
+      
+      paddingTop: 35,
+      paddingBottom: 65,
+      paddingHorizontal: 35,
     },
     section: {
-      display: 'flex',
       margin: 10,
       padding: 10,
-      flexGrow: 1
-    },
-    text: {
-      display: 'block',
-      fontSize: 20
+      flexGrow: 1,
+      textAlign: "justify",
+      marginTop: 20,
+      fontSize: 20,
+      fontFamily: 'Helvetica'
     },
     imageSize: {
-      width: 150,
-      
-    }
-  })
+      marginVertical: 15,
+      marginHorizontal: 100,
+    },
+    title: {
+      fontSize: 24,
+      textAlign: "center",
+      fontFamily: "Helvetica",
+      margin: 20
+    },
+    pageNumber: {
+    position: 'absolute',
+    fontSize: 12,
+    bottom: -30,
+    left: 0,
+    right: 0,
+    textAlign: 'center',
+    color: 'grey',
+  },
+  });
   return (
     <Document>
-      <Page size={"A4"} style={styles.page}>
+      <Page style={styles.page}>
         <View style={styles.section}>
-        <Image src={img} style={styles.imageSize}/>
-          <Text>Download</Text>
-        <Text>
-          Aliqua voluptate ea do ea Lorem reprehenderit cillum amet tempor.
-          Deserunt incididunt pariatur proident mollit ea sunt nisi. Magna ipsum
-          cupidatat labore officia labore. Id labore ad eu cillum adipisicing
-          aute dolore. Ut incididunt aute labore ea consequat eiusmod mollit
-          aliquip cillum deserunt. Amet incididunt Lorem incididunt aliquip.
-        </Text>
+          <Image src={img} style={styles.imageSize} />
+          <Text style={styles.title}>Text Download</Text>
+          <Text>
+            Aliqua voluptate ea do ea Lorem reprehenderit cillum amet tempor.
+            Deserunt incididunt pariatur proident mollit ea sunt nisi. Magna
+            ipsum cupidatat labore officia labore. Id labore ad eu cillum
+            adipisicing aute dolore. Ut incididunt aute labore ea consequat
+            eiusmod mollit aliquip cillum deserunt. Amet incididunt Lorem
+            incididunt aliquip.
+          </Text>
+          <Text>
+            Aliqua voluptate ea do ea Lorem reprehenderit cillum amet tempor.
+            Deserunt incididunt pariatur proident mollit ea sunt nisi. Magna
+            ipsum cupidatat labore officia labore. Id labore ad eu cillum
+            adipisicing aute dolore. Ut incididunt aute labore ea consequat
+            eiusmod mollit aliquip cillum deserunt. Amet incididunt Lorem
+            incididunt aliquip.
+          </Text>
+          <Text>
+            Aliqua voluptate ea do ea Lorem reprehenderit cillum amet tempor.
+            Deserunt incididunt pariatur proident mollit ea sunt nisi. Magna
+            ipsum cupidatat labore officia labore. Id labore ad eu cillum
+            adipisicing aute dolore. Ut incididunt aute labore ea consequat
+            eiusmod mollit aliquip cillum deserunt. Amet incididunt Lorem
+            incididunt aliquip.
+          </Text>
+          <Text>
+            Aliqua voluptate ea do ea Lorem reprehenderit cillum amet tempor.
+            Deserunt incididunt pariatur proident mollit ea sunt nisi. Magna
+            ipsum cupidatat labore officia labore. Id labore ad eu cillum
+            adipisicing aute dolore. Ut incididunt aute labore ea consequat
+            eiusmod mollit aliquip cillum deserunt. Amet incididunt Lorem
+            incididunt aliquip.
+          </Text>
+          <Text>
+            Aliqua voluptate ea do ea Lorem reprehenderit cillum amet tempor.
+            Deserunt incididunt pariatur proident mollit ea sunt nisi. Magna
+            ipsum cupidatat labore officia labore. Id labore ad eu cillum
+            adipisicing aute dolore. Ut incididunt aute labore ea consequat
+            eiusmod mollit aliquip cillum deserunt. Amet incididunt Lorem
+            incididunt aliquip.
+          </Text>
+          <Text>
+            Aliqua voluptate ea do ea Lorem reprehenderit cillum amet tempor.
+            Deserunt incididunt pariatur proident mollit ea sunt nisi. Magna
+            ipsum cupidatat labore officia labore. Id labore ad eu cillum
+            adipisicing aute dolore. Ut incididunt aute labore ea consequat
+            eiusmod mollit aliquip cillum deserunt. Amet incididunt Lorem
+            incididunt aliquip.
+          </Text>
+          <Text>
+            Aliqua voluptate ea do ea Lorem reprehenderit cillum amet tempor.
+            Deserunt incididunt pariatur proident mollit ea sunt nisi. Magna
+            ipsum cupidatat labore officia labore. Id labore ad eu cillum
+            adipisicing aute dolore. Ut incididunt aute labore ea consequat
+            eiusmod mollit aliquip cillum deserunt. Amet incididunt Lorem
+            incididunt aliquip.
+          </Text>
+          <Text>
+            Aliqua voluptate ea do ea Lorem reprehenderit cillum amet tempor.
+            Deserunt incididunt pariatur proident mollit ea sunt nisi. Magna
+            ipsum cupidatat labore officia labore. Id labore ad eu cillum
+            adipisicing aute dolore. Ut incididunt aute labore ea consequat
+            eiusmod mollit aliquip cillum deserunt. Amet incididunt Lorem
+            incididunt aliquip.
+          </Text>
+          <Text>
+            Aliqua voluptate ea do ea Lorem reprehenderit cillum amet tempor.
+            Deserunt incididunt pariatur proident mollit ea sunt nisi. Magna
+            ipsum cupidatat labore officia labore. Id labore ad eu cillum
+            adipisicing aute dolore. Ut incididunt aute labore ea consequat
+            eiusmod mollit aliquip cillum deserunt. Amet incididunt Lorem
+            incididunt aliquip.
+          </Text>
+          <Text>
+            Aliqua voluptate ea do ea Lorem reprehenderit cillum amet tempor.
+            Deserunt incididunt pariatur proident mollit ea sunt nisi. Magna
+            ipsum cupidatat labore officia labore. Id labore ad eu cillum
+            adipisicing aute dolore. Ut incididunt aute labore ea consequat
+            eiusmod mollit aliquip cillum deserunt. Amet incididunt Lorem
+            incididunt aliquip.
+          </Text>
         </View>
         <View>
-        <Text render={({pageNumber, totalPages}) => `${pageNumber} / ${totalPages}`}/>
+          <Text
+          style={styles.pageNumber}
+            render={({ pageNumber, totalPages }) =>
+              `${pageNumber} / ${totalPages}`
+            }
+            fixed
+          />
         </View>
       </Page>
     </Document>
