c5216d292d08dc9e8c85c1e404b09720727303b3
JLJL <juanluisjaramillolopez@gmail.com>
2024-02-18 19:38:14 -0600

    creacion del documento de pdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+//para poder descargar el archivo de pdf creado necesitamos llamara una funcion de react-pdf
+import { PDFDownloadLink } from '@react-pdf/renderer'
+import PdfDocument from './components/PdfDocument'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <PDFDownloadLink document={<PdfDocument/>} fileName='ejemplo.pdf'>
+        {
+          //dentro creamos validacione para la descarga del atchivo //, url, error, blob
+          ({ loading }) => loading? <p>Loading...</p> : <button>Download</button>
+        }
+      </PDFDownloadLink>
+
+    </div>
   )
 }
 
